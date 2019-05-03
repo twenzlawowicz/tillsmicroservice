d16d2049b2a075cdc21cb20824cc4a22af16dedc
Till von Wenzlawowicz <till.vonwenzlawowicz@eventim.de>
2019-05-03 16:50:12 +0200

    Pen-Beispiel

---
+++ tillsMicroservice/Controllers/Pen.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.IdentityModel.Tokens;
+
+namespace tillsMicroservice
+{
+    public enum PenColor
+    {
+        Red,
+        Blue
+    };
+
+
+    public class Pen
+    {
+        public PenColor Color { get; set; }
+
+        private Pen(PenColor _color)
+        {
+            Color = _color;
+        }
+
+        public static Pen CreateRedPen()
+        {
+            return new Pen(PenColor.Red);
+        }
+
+        public static Pen CreateBluePen()
+        {
+            return new Pen(PenColor.Blue);
+        }
+
+        public string ToJson()
+        {
+            string json = "{\"pen\": {\"color\": \"";
+            switch (Color)
+            {
+                case PenColor.Red:
+                    json += "red";
+                    break;
+                case PenColor.Blue:
+                    json += "blue";
+                    break;
+            }
+
+            json += "\"}}";
+
+            return json;
+        }
+
+    };
+}
--- tillsMicroservice/Controllers/ValuesController.cs
+++ tillsMicroservice/Controllers/ValuesController.cs
@@ -10,44 +10,30 @@ namespace tillsMicroservice.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        private InMemoryDB _db;
         public ValuesController()
         {
-            _db = new InMemoryDB();
         }
 
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] {_db.Count().ToString() };
-        }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(string id)
         {
-            return _db.Read(id);
+
+            Pen p;
+            switch (id)
+            {
+                case "red":
+                    p = Pen.CreateRedPen();
+                    return p.ToJson();
+                case "blue":
+                    p = Pen.CreateBluePen();
+                    return p.ToJson();
+            }
+
+            return "No pen";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-            _db.Insert(value);
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(string id, [FromBody] string value)
-        {
-            _db.Save(id,value);
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
